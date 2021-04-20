5c857d096a6ac75ebfb7299167f0a7772d1d72e5
IgorPV <igor.pereira.vieira@gmail.com>
2021-04-20 15:17:21 -0300

    zoom no mapa, nova fonte e novo layout de icones em contato

--- fundamentos-web/assets/js/script.js
+++ fundamentos-web/assets/js/script.js
@@ -2,6 +2,10 @@
 let nome = document.querySelector("input#nome")
 let email = document.querySelector("input#email")
 let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector(".mapsGoogle")
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -11,8 +15,10 @@ function validarNome(){
     if(nome.value.length<2){
         avisoNome.innerHTML = "Nome Inválido!"
         avisoNome.style.color = "red"
+        avisoNome.style.display = "block"
     } else {
-        avisoNome.innerHTML = null
+        avisoNome.style.display = "none"
+        nomeOk = true
     }
 }
 
@@ -21,11 +27,42 @@ function validarEmail(){
     if(email.value.indexOf("@") == -1){
         avisoEmail.innerHTML = "Email Inválido!"
         avisoEmail.style.color = "red"
+        avisoEmail.style.display = "block"
     } else {
-        avisoEmail.innerHTML = null
+        avisoEmail.style.display = "none"
+        emailOk = true
     }
 }
 
+function validarAssunto(){
+    let avisoAssunto = document.querySelector("#avisoAssunto")
+    if(assunto.value.length>240){
+        avisoAssunto.innerHTML = "Texto muito grande. Digite no máx 100 caracteres"
+        avisoAssunto.style.color = "red"
+        avisoAssunto.style.display = "block"
+    } else{
+        avisoAssunto.style.display = "none"
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert("Formulário enviado com sucesso!")
+    }else{
+        alert("Preencha o formulário corretamente")
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+
+function mapaNormal(){
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
+}
 
 function openNav() {
     document.querySelector("#menu-principal").style.width = "250px";
